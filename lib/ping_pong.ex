f3881f27ebf30f67d7a0f8f538cd6ca7eece9de5
Pavel Vlasikhin <pavel@vlasikhin.com>
2024-10-01 20:00:08 +0200

    update: state removed

--- lib/ping_pong.ex
+++ lib/ping_pong.ex
@@ -3,31 +3,31 @@ defmodule PingPong do
 
   @moduledoc """
   A simple echo server implemented using `GenServer`. This module responds to a `:ping`
-  message with a tuple `{:pong, node()}`, where `node()` represents the current node.
+  message with `:pong`.
 
   # Usage
 
   To start the GenServer send a ping and receive a pong:
       iex> {:ok, _pid} = PingPong.start_link()
       iex> PingPong.ping()
-      {:pong, node()}
+      :pong
   """
 
-  def start_link() do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  def start_link do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  def ping() do
+  def ping do
     GenServer.call(__MODULE__, :ping)
   end
 
   @impl true
-  def init(state) do
-    {:ok, state}
+  def init(_) do
+    {:ok, nil}
   end
 
   @impl true
   def handle_call(:ping, _from, state) do
-    {:reply, {:pong, node()}, state}
+    {:reply, :pong, state}
   end
 end
--- test/ping_pong_test.exs
+++ test/ping_pong_test.exs
@@ -2,9 +2,8 @@ defmodule PingPongTest do
   use ExUnit.Case
   doctest PingPong
 
-  test "returns {:pong, node()} when ping is called" do
+  test "returns :pong when ping is called" do
     {:ok, _pid} = PingPong.start_link()
-
-    assert PingPong.ping() == {:pong, node()}
+    assert PingPong.ping() == :pong
   end
 end
